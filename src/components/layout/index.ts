621fa969efd5253ea7b43fc94ce7b1d542bc0edd
Jilver Pacheco <jilverrivera1@gmail.com>
2024-11-11 00:06:22 -0500

    feat: deploy v1

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,5 +1,5 @@
 export * from './article-layout';
 export * from './container.component';
 export * from './footer.component';
-export * from './header';
+export * from './header.component';
 export * from './main-layout';
